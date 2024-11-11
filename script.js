a0573bf54bfc6595f1f4c675342faced75190ead
Thalles Lorrhan S. da Motta <thalles.lorrhan30@gmail.com>
2024-11-11 15:42:20 -0300

    changes in ass, log and sing

--- script.js
+++ script.js
@@ -19,3 +19,30 @@ document.addEventListener("DOMContentLoaded", function() {
     var imgLink = document.querySelector(".img01 a");
     imgLink.addEventListener("click", myFunction);
 });
+
+// script.js
+function toggleModal() {
+    const modal = document.getElementById('acessibilidade-modal');
+    modal.style.display = modal.style.display === 'flex' ? 'none' : 'flex';
+}
+
+// Funções para aumentar e diminuir o tamanho da fonte
+function aumentarFonte() {
+    document.body.style.fontSize = '20px';
+}
+
+function diminuirFonte() {
+    document.body.style.fontSize = '10px';
+}
+
+// Função para abrir/fechar o modal de login
+function toggleLog() {
+    const modal = document.getElementById('log');
+    modal.style.display = modal.style.display === 'flex' ? 'none' : 'flex';
+}
+
+// Função para abrir/fechar o modal de cadastro
+function toggleSing() {
+    const modal = document.getElementById('sing');
+    modal.style.display = modal.style.display === 'flex' ? 'none' : 'flex';
+}
